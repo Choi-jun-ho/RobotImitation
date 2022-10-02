0c327f0cd7f89246c71942025a6ab9ef60e7fdb4
Choi-jun-ho <wnsgh7458969@gmail.com>
2022-10-03 00:58:49 +0900

    Refactor: 클래스를 함수로 변환

--- Angle.py
+++ Angle.py
@@ -3,106 +3,96 @@
 import math
 import almath
 
+def to_radian(angle):
+    return angle*almath.TO_RAD
 
-class AngleMath:
+def to_degree(radian):
+    return radian*almath.TO_DEG
 
-    @staticmethod
-    def to_radian(angle):
-        return angle*almath.TO_RAD
+def to_middle_xy(a, b):
+    """a를 중점으로 하는 b의 좌표를 리턴
 
-    @staticmethod
-    def to_degree(radian):
-        return radian*almath.TO_DEG
+    :param a:
+        almath.Position2D 객체
+    :param b:
+        almath.Position2D 객체
+    :return:
+        b - a
+    """
+    return b - a
 
-    @staticmethod
-    def to_middle_xy(a, b):
-        """a를 중점으로 하는 b의 좌표를 리턴
+def get_to_radian(a, b):
+    """라디안값을 추출하는 함수 리턴
 
-        :param a:
-            almath.Position2D 객체
-        :param b:
-            almath.Position2D 객체
-        :return:
-            b - a
-        """
-        return b - a
+    :param a:
+        almath.Position2D 객체
+    :param b:
+        almath.Position2D 객체
+    :return:
+        -pi~+pi 를 가지는 각도로 반환(거리 없음) 반환
+    """
+    r = to_middle_xy(a, b)
 
-    @staticmethod
-    def get_to_radian(a, b):
-        """라디안값을 추출하는 함수 리턴
+    x, y = r.x, r.y
 
-        :param a:
-            almath.Position2D 객체
-        :param b:
-            almath.Position2D 객체
-        :return:
-            -pi~+pi 를 가지는 각도로 반환(거리 없음) 반환
-        """
-        r = to_middle_xy(a, b)
+    if x > 0:
+        return math.atan(y / x)
+    elif x < 0 and y >= 0:
+        return math.atan(y / x) + math.pi
+    elif x < 0 and y < 0:
+        return math.atan(y / x) - math.pi
+    elif x == 0 and y > 0:
+        return math.pi / 2
+    elif x == 0 and y < 0:
+        return -math.pi / 2
+    else:
+        return 0
 
-        x, y = r.x, r.y
+def get_to_radian2d(a, b):
+    """
+    라디안값을 추출하는 함수 리턴 0 ~ 2pi
+    :param a:
+    :param b:
+    :return:
+        0 ~ 2pi를 가지는 각도로 반환(거리 없음) 반환
+    """
+    r = to_middle_xy(a, b)
 
-        if x > 0:
-            return math.atan(y / x)
-        elif x < 0 and y >= 0:
-            return math.atan(y / x) + math.pi
-        elif x < 0 and y < 0:
-            return math.atan(y / x) - math.pi
-        elif x == 0 and y > 0:
-            return math.pi / 2
-        elif x == 0 and y < 0:
-            return -math.pi / 2
-        else:
-            return 0
+    x, y = r.x, r.y
 
-    @classmethod
-    def get_to_radian2d(cls, a, b):
-        """
-        라디안값을 추출하는 함수 리턴 0 ~ 2pi
-        :param a:
-        :param b:
-        :return:
-            0 ~ 2pi를 가지는 각도로 반환(거리 없음) 반환
-        """
-        r = cls.to_middle_xy(a, b)
+    if x > 0 and y >= 0:
+        return math.atan(y / x)
+    elif x > 0 and y < 0:
+        return math.atan(y / x) + math.pi * 2
+    elif x < 0:
+        return math.atan(y / x) + math.pi
+    elif x == 0 and y > 0:
+        return math.pi / 2
+    elif x == 0 and y < 0:
+        return math.pi * 3 / 2
+    else:
+        return -1
 
-        x, y = r.x, r.y
+def reversal_way_angle(angle):
+    """기준점의 시작 방향을 반대로 바꾼다
+    :param angle:   각도
+    :return: 2pi-angle
 
-        if x > 0 and y >= 0:
-            return math.atan(y / x)
-        elif x > 0 and y < 0:
-            return math.atan(y / x) + math.pi * 2
-        elif x < 0:
-            return math.atan(y / x) + math.pi
-        elif x == 0 and y > 0:
-            return math.pi / 2
-        elif x == 0 and y < 0:
-            return math.pi * 3 / 2
-        else:
-            return -1
+    """
+    return math.pi * 2 - angle
 
-    @staticmethod
-    def reversal_way_angle(angle):
-        """기준점의 시작 방향을 반대로 바꾼다
-        :param angle:   각도
-        :return: 2pi-angle
+def change_zero_benchmark(angle, change_angle):
+    """극 좌표계 angle의 0의 기준을 changeAngle로 변환한다
+    :param angle:
+    :param change_angle:
+    :return:    좌표계의 angle의 기준을 changeAngle로 바꾼 값을 전달
+    """
+    angle -= change_angle
 
-        """
-        return math.pi * 2 - angle
+    if angle < 0:
+        angle = 2 * math.pi + angle
 
-    @staticmethod
-    def change_zero_benchmark(angle, change_angle):
-        """극 좌표계 angle의 0의 기준을 changeAngle로 변환한다
-        :param angle:
-        :param change_angle:
-        :return:    좌표계의 angle의 기준을 changeAngle로 바꾼 값을 전달
-        """
-        angle -= change_angle
+    if angle >= 2 * math.pi:
+        angle -= 2 * math.pi
 
-        if angle < 0:
-            angle = 2 * math.pi + angle
-
-        if angle >= 2 * math.pi:
-            angle -= 2 * math.pi
-
-        return angle
+    return angle
