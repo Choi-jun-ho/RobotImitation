ba0865d2a6123181674ac20c8257b54437ca71bf
Choi-jun-ho <wnsgh7458969@gmail.com>
2022-10-08 00:32:02 +0900

    Refactor: to_nao_shourder_radian2d 함수를 to_nao_roll_radian2d로 변경
    알고보니까 제로 벤치 였을때 기준을 정했을때 전체 roll에 대해 변경하는 함수였음
    몸통 - 어깨 하면 roll 환경 상에서 안정될 듯 보임

--- nao_to_angle.py
+++ nao_to_angle.py
@@ -6,13 +6,12 @@ nao를 좌표로 움직일 수 있게 할 수 있게 하는 모듈
 
 """
 import time
-
 from AngleMath import *
 import numpy as np
 import math
 
 
-def to_nao_shourlder_radain2d(a, b):
+def to_nao_roll_radain2d(a, b):
     """다리가 고정인 상태에서 팔을 움직일 수 있게함
 
     :param a:
@@ -21,8 +20,14 @@ def to_nao_shourlder_radain2d(a, b):
         nao 좌표계로 기준을 정렬을 한다
     """
     angle = get_to_radian2d(a, b)
-    angle = change_zero_benchmark(angle, math.pi * 3 / 2)
+    # 몸체 각도
+
+    # 몸체 각도 - 팔 각도 -> 저장 필요
+
+    angle = zero_benchmark_roll(angle)
     # 기준점 정렬 완료 상태, +- 상태로 변환 해야 된다
+    print angle*almath.TO_DEG
+
     if angle <= math.pi:
         return angle
     else:
@@ -44,8 +49,8 @@ def before_now_get_power(before_a, before_b, now_a, now_b):
     :return:
         power:nao에게 주어진 power, absolute_value:|before_angle - now_angle|
     """
-    before_angle = to_nao_shourlder_radain2d(before_a, before_b)
-    now_angle = to_nao_shourlder_radain2d(now_a, now_b)
+    before_angle = to_nao_roll_radain2d(before_a, before_b)
+    now_angle = to_nao_roll_radain2d(now_a, now_b)
     absolute_value = before_angle - now_angle if before_angle > now_angle else now_angle - before_angle
     power = float(hyperbolicTan(absolute_value))
     return power, absolute_value
@@ -62,7 +67,7 @@ def action_r_shourlder(a, b, sleep_time, motion_service, power):
     :return:
     """
 
-    angle = to_nao_shourlder_radain2d(a, b)
+    angle = to_nao_roll_radain2d(a, b)
 
     if angle >= to_radian(18):
         motion_service.setAngles("RShoulderRoll", to_radian(18), power)
@@ -86,7 +91,7 @@ def action_l_shourlder(a, b, sleep_time, motion_service, power):
     :param power: 
     :return: 
     """
-    angle = to_nao_shourlder_radain2d(a, b)
+    angle = to_nao_roll_radain2d(a, b)
 
     if angle >= to_radian(76):
         motion_service.setAngles("LShoulderRoll", to_radian(76), power)
